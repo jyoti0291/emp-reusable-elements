421f1300b31220b2fa137844e0feb1de80fda539
Jyoti Singh <jyoti.singh@rakuten.com>
2020-05-07 13:09:59 +0530

    EMPPSN-23012

--- src/app/shared/components/form-builder/form-builder.component.ts
+++ src/app/shared/components/form-builder/form-builder.component.ts
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-builder.component.scss']
 })
 export class FormBuilderComponent implements OnInit {
-  public formSrc = {components: []};
+  public formSrc = { components: [] };
+  public formOptions = {
+    builder: {
+      basic: {
+        components: {
+          time: false,
+          password: false,
+          phoneNumber: false,
+          number: false,
+          content: false,
+          selectboxes: false,
+        }
+      },
+      advanced: false,
+      data: false,
+      layout: false
+    },
+    editForm: {
+      textfield: [
+        {
+          key: 'api',
+          ignore: true
+        },
+        {
+          key: 'layout',
+          ignore: true
+        },
+        {
+          key: 'conditional',
+          ignore: true
+        },
+        {
+          key: 'logic',
+          ignore: true
+        }
+      ]
+    }
+  }
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChange(event) {
   }
 
 }
