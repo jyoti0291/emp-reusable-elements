03b852a2e9bc6a74414c20be7b9299481d390240
Jyoti Singh <jyoti.singh>
2020-09-11 11:15:31 +0530

    name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('emp-user-interface app is running!');
+    expect(page.getTitleText()).toEqual('emp-reusable-elements app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/emp-user-interface'),
+      dir: require('path').join(__dirname, './coverage/emp-reusable-elements'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
